266f3b8481bd51220b931e8c03d5cfa6348d230e
ncrout1 <ncrout1999@gmail.com>
2025-04-24 15:12:54 +0530

    update in the script

--- first_question.py
+++ first_question.py
@@ -19,3 +19,20 @@ for i in range(1,len(l)-1):
 print(l)
 
 [594, 1284, 5671, 1765, 23, 145612]
+
+
+l=[1284, 145612, 5671, 23, 1765, 594]
+for i in range(len(l)-1):
+    
+    
+    for j in range(i+1,len(l)):
+        # print("Hello")
+        prev_num=list(str(l[i]))[-2]
+        num=list(str(l[j]))[-2]
+        
+        if(num>prev_num):
+            temp=l[j]
+            l[j]=l[i]
+            l[i]=temp
+print(l)
+        
